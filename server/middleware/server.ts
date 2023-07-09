a2c94852fb9d9efb228719d36f6c74088b99737e
Cyan903 <cyanp0515@gmail.com>
2023-07-09 09:45:35 -0400

    chore: Simplify prisma system status

--- server/middleware/server.ts
+++ server/middleware/server.ts
@@ -2,7 +2,7 @@ import prisma from "@/server/data/prisma";
 
 export default defineEventHandler(async (evt) => {
     try {
-        evt.context.settings = await prisma.system.findFirst();
+        evt.context.settings = await prisma.system.findFirstOrThrow();
     } catch (err) {
         process.stdout.write("[FATAL] Could not fetch system settings!");
         process.stderr.write(String(err));
