db0c4c4d106db2c03dc31f7f408c25107706c37f
Cyan903 <cyanp0515@gmail.com>
2023-08-15 12:37:16 -0400

    feat: Improve navbar for updated links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,75 +1,91 @@
 <template>
     <div v-if="!isFatal" class="navbar navbar-floating sticky rounded-lg">
         <div class="navbar-start">
-            <div v-if="auth" class="avatar avatar-ring avatar-md">
+            <div :class="{ 'avatar avatar-ring avatar-md': auth }">
                 <div class="dropdown-container">
                     <div class="dropdown">
                         <label
                             class="btn btn-ghost flex cursor-pointer px-0"
+                            :class="{ 'px-2': !auth }"
                             tabindex="0"
                         >
-                            <img :src="data?.user?.image || ''" alt="avatar" />
+                            <Bars3Icon v-if="!auth" class="w-6 h-6" />
+                            <img
+                                v-else
+                                :src="data?.user?.image || ''"
+                                alt="avatar"
+                            />
                         </label>
+
                         <div class="dropdown-menu dropdown-menu-bottom-right">
-                            <NuxtLink
-                                to="/"
-                                class="dropdown-item text-sm md:hidden inline"
-                            >
+                            <NuxtLink to="/" class="dropdown-item text-sm">
                                 Home
+                            </NuxtLink>
+
+                            <NuxtLink to="/about" class="dropdown-item text-sm">
+                                About
                             </NuxtLink>
 
                             <NuxtLink
                                 to="/products"
-                                class="dropdown-item text-sm md:hidden inline"
+                                class="dropdown-item text-sm"
                             >
                                 Products
                             </NuxtLink>
 
-                            <hr class="md:hidden inline" />
+                            <hr />
 
-                            <NuxtLink
-                                to="/profile"
-                                class="dropdown-item text-sm"
-                            >
-                                My Profile
-                            </NuxtLink>
+                            <template v-if="auth">
+                                <NuxtLink
+                                    to="/profile"
+                                    class="dropdown-item text-sm"
+                                >
+                                    My Profile
+                                </NuxtLink>
 
-                            <NuxtLink
-                                to="/profile/orders"
-                                tabindex="-1"
-                                class="dropdown-item text-sm"
-                            >
-                                Orders
-                            </NuxtLink>
+                                <NuxtLink
+                                    to="/profile/orders"
+                                    tabindex="-1"
+                                    class="dropdown-item text-sm"
+                                >
+                                    Orders
+                                </NuxtLink>
 
-                            <NuxtLink
-                                to="/profile/ratings"
-                                tabindex="-1"
-                                class="dropdown-item text-sm"
-                            >
-                                Ratings
-                            </NuxtLink>
+                                <NuxtLink
+                                    to="/profile/ratings"
+                                    tabindex="-1"
+                                    class="dropdown-item text-sm"
+                                >
+                                    Ratings
+                                </NuxtLink>
 
-                            <hr class="md:hidden inline" />
-                            <a
-                                class="dropdown-item text-sm md:hidden inline"
-                                @click="logout"
-                            >
-                                Logout
-                            </a>
+                                <hr />
+                                <a
+                                    class="dropdown-item text-sm"
+                                    @click="logout"
+                                >
+                                    Logout
+                                </a>
+                            </template>
+                            <template v-else>
+                                <a
+                                    class="dropdown-item text-sm"
+                                    @click="signIn('auth0')"
+                                >
+                                    Login
+                                </a>
+
+                                <a
+                                    class="dropdown-item text-sm"
+                                    @click="signIn('auth0')"
+                                >
+                                    Register
+                                </a>
+                            </template>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <!-- Hide if auth and md -->
-            <NuxtLink
-                to="/products"
-                class="navbar-item"
-                :class="{ 'md:inline hidden': auth }"
-            >
-                <BuildingStorefrontIcon class="h-6 w-6" />
-            </NuxtLink>
         </div>
 
         <div class="navbar-center">
@@ -87,25 +103,8 @@
                 </div>
             </template>
             <template v-else>
-                <div
-                    class="navbar-item md:inline hidden"
-                    @click="signIn('auth0')"
-                >
+                <div class="navbar-item" @click="signIn('auth0')">
                     <UserPlusIcon class="h-6 w-6" />
-                </div>
-
-                <div
-                    class="navbar-item md:inline hidden"
-                    @click="signIn('auth0')"
-                >
-                    <ArrowLeftOnRectangleIcon class="h-6 w-6" />
-                </div>
-
-                <div
-                    class="navbar-item md:hidden inline"
-                    @click="signIn('auth0')"
-                >
-                    <UserGroupIcon class="h-6 w-6" />
                 </div>
             </template>
         </div>
@@ -114,9 +113,8 @@
 
 <script lang="ts" setup>
 import {
-    ArrowLeftOnRectangleIcon,
+    Bars3Icon,
     UserPlusIcon,
-    UserGroupIcon,
     BuildingStorefrontIcon,
     ArrowRightOnRectangleIcon,
 } from "@heroicons/vue/24/outline";
